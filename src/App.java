a7878baa22039b5458881f9ce27d292e1e242761
Michael <michaelyumbla6@gmail.com>
2025-07-20 13:44:44 -0500

    Second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import model.HotelDatabase;
+import model.SQLiteHotelDatabase; // Implementación concreta
+import view.LoginView;
+import controller.LoginController;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Inicializar la base de datos
+        HotelDatabase database = new SQLiteHotelDatabase();
+        
+        // Inicializar la vista de login
+        LoginView loginView = new LoginView();
+        
+        // Inicializar el controlador
+        new LoginController(loginView, database);
+        
+        // Mostrar la vista
+        loginView.setVisible(true);
     }
 }
